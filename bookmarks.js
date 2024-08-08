d979384498607589b28b082cd5bc2cc58ae94401
rohnkhatri <khatrirohan221@gmail.com>
2024-08-08 19:12:37 +0530

    bookmarks updated

--- bookmarks.js
+++ bookmarks.js
@@ -1,40 +1,20 @@
 // Note: having length != 4 will mess with layout based on how the site is styled
 const bookmarks = [
   {
-    title: "Daily",
+    title: "Programming",
     links: [
-      { name: "Inbox", url: "https://inbox.google.com" },
+      { name: "LeetCode", url: "https://leetcode.com/problemset/" },
       { name: "GitHub", url: "https://github.com" },
-      { name: "Drive", url: "https://drive.google.com" },
+      { name: "ChatGTP", url: "https://chatgpt.com/" },
+      { name: "HuggingChat", url: "https://huggingface.co/chat/"},
     ],
   },
   {
     title: "Media",
     links: [
       { name: "Youtube", url: "https://youtube.com" },
-      { name: "Netflix", url: "https://netflix.com" },
-      { name: "Instagram", url: "https://instagram.com" },
-      {
-        name: "Amazon Prime",
-        url: "https://www.amazon.com/Amazon-Video",
-      },
-    ],
-  },
-  {
-    title: "Reddit",
-    links: [
-      { name: "/r/overwatch", url: "https://reddit.com/r/overwatch" },
-      {
-        name: "/r/pcmasterrace",
-        url: "https://reddit.com/r/pcmasterrace",
-      },
-      { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
-      {
-        name: "/r/battlestations",
-        url: "https://reddit.com/r/battlestations",
-      },
-      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
-      { name: "/r/news", url: "https://reddit.com/r/news" },
+      { name: "PirateBay", url: "https://piratebayproxy.net/" },
+      // { name: "Instagram", url: "https://instagram.com" },
     ],
   },
   {
@@ -42,6 +22,21 @@ const bookmarks = [
     links: [
       { name: "Twitter", url: "https://twitter.com" },
       { name: "Facebook", url: "https://facebook.com" },
+      { name: "Instagram", url: "https://instagram.com" },
+      { name: "Reddit", url: "https://reddit.com" },
+    ],
+  },
+  {
+    title: "Miscellaneous",
+    links: [
+      { name: "GFG", url: "https://www.geeksforgeeks.org/explore?page=1&sortBy=submissions" },
+      { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
+      {
+        name: "/r/battlestations",
+        url: "https://reddit.com/r/battlestations",
+      },
+      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
+      { name: "/r/news", url: "https://reddit.com/r/news" },
     ],
   },
 ];
